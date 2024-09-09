36474467d7b0ea9ffb81197a83899559f60851d8
victorribeirog <vicrgon2010@hotmail.com>
2024-09-09 19:52:07 -0300

    .

---
+++ AppMSM/src/main/java/apps/MSNMessenger.java
@@ -0,0 +1,15 @@
+package apps;
+
+public class MSNMessenger extends ServicoMensagemInstantanea{
+
+    @Override
+    public void enviarMensagem() {
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem pelo MSN Messenger");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN Messenger");
+    }
+}
---
+++ AppMSM/src/main/java/apps/TelegramMessenger.java
@@ -0,0 +1,14 @@
+package apps;
+
+public class TelegramMessenger extends ServicoMensagemInstantanea {
+    @Override
+    public void enviarMensagem() {
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem pelo Telegram");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Telegram");
+    }
+}
